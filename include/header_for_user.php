7fddeba9f0d9bb0df1047cce40c5333cb1d25027
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2024-09-03 13:44:04 +0100

    navigation(html+css)
    creation navigation

---
+++ include/header_for_user.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>vomie - <?=$title?></title>
+		<link rel="icon" href="styles/logo.jpg" type="image/x-icon" />
+		<meta name="viewport" content="width=device-width, intial-scale=1.0">
+		<meta http-equiv="cache-control" content="no-cache">
+		<meta http-equiv="expires" content="0">
+		<meta http-equiv="pragma" content="no-cache">
+		<link rel="stylesheet" href="styles/main.css" />
+	</head>
+	<body>
+		<nav class="navbar">
+			<div class="logo">vomie</div>
+			<ul class="menu">
+				<li class="menu-item">
+					<a href="#" class="menu-link">Home</a>
+					<ul class="dropdown-menu">
+						<li><a href='#'> Movies </a></li>
+						<li><a href='#'> Movies </a></li>
+						<li><a href='#'> Movies </a></li>
+						<li><a href='#'> Movies </a></li>
+					</ul>
+				</li>
+			</ul>
+			<div class="right-section">
+				<button class="search-btn">Search</button>
+				<img src="avatar.jpg" alt="User Avatar" class="avatar">
+			</div>
+		</nav>
+	</body>
+</html>
