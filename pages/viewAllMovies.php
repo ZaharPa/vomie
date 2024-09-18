a29f085b04d3981e14a4d3f791a4f611fa2e2388
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2024-09-18 13:17:51 +0100

    Countinied css js viewAllMovies (4 part)

--- pages/viewAllMovies.php
+++ pages/viewAllMovies.php
@@ -21,71 +21,75 @@
 <div class="views-all">
  	<div class="container">
  		<div class="search-bar">
- 			<input type="text" placeholder="Input here name anime">
+ 			<input type="text" placeholder="Input here name movie">
  		</div>
  	</div>
  	
- 	<div class="filters">
-		<div class="filter-section">
- 			<button class="filter-toggle">Genre</button>
- 			<div class="filter-options">
- 				<span class="filter-option">horror</span>
- 				<span class="filter-option">drama</span>
- 				<span class="filter-option">science</span>
- 				<span class="filter-option">comedy</span>
- 				<span class="filter-option">action</span>
- 				<span class="filter-option">documentary</span>
- 				<span class="filter-option">fantasy</span>
- 				<span class="filter-option">musical</span>
- 				<span class="filter-option">sports</span>
- 				<span class="filter-option">romance</span>
- 				<span class="filter-option">thriller</span>
- 				<span class="filter-option">spy</span>
- 				<span class="filter-option">crime</span>
- 			</div>
- 		</div>
+ 	<div class="movie-section">
  		
- 		<div class="filter-section">
- 			<button class="filter-toggle">Status</button>
- 			<div class="filter-options">
- 				<span class="filter-option">movie</span>
- 				<span class="filter-option">serial</span>
- 				<span class="filter-option">cartoon movie</span>
- 				<span class="filter-option">cartoon series</span>
- 				<span class="filter-option">anime</span>
- 			</div>
- 		</div>
- 		
- 		<div class="filter-section">
- 			<button class="filter-toggle">Year</button>
- 			<div class="year-slider">
- 				<input type="number" id="year-min" value="1900" readonly>
- 				<div id="slider"></div>
- 				<input type="number" id="year-max" value="2024" readonly>
- 			</div>
- 		</div>
- 	</div>
- 	
- 	<div class="movie-list">
- 		<?php  for ($i = $startIndex; $i < $endIndex; $i++) { 
- 		    for($x = 0; $x < count($photoMovie); $x++) {
- 		        if($photoMovie[$x]['id_movie'] === $movies[$i]['id_movie']) {
- 		             $imgSrc = $photoMovie[$x]['path'] . $photoMovie[$x]['photo'];
- 		             break;
- 		        }
- 		    }?>
-     		<div class="movie-card">
-     			<img src='<?=$imgSrc?>' alt="Name movie">
-     			<p><?=htmlspecialchars($movies[$i]['name'])?></p>
+ 		<div class="movie-list">
+     		<?php for ($i = $startIndex; $i < $endIndex; $i++) { 
+     		    for($x = 0; $x < count($photoMovie); $x++) {
+     		        if($photoMovie[$x]['id_movie'] === $movies[$i]['id_movie']) {
+     		             $imgSrc = $photoMovie[$x]['path'] . $photoMovie[$x]['photo'];
+     		             break;
+     		        }
+     		    }?>
+         		<div class="movie-card">
+         			<img src='<?=$imgSrc?>' alt="Name movie">
+         			<p><?=htmlspecialchars($movies[$i]['name'])?></p>
+         		</div>
+         	<?php }?>
+         </div>
+     	
+     	<div class="filters">
+    		<div class="filter-section">
+     			<button class="filter-toggle">Genre</button>
+     			<div class="filter-options">
+     				<span class="filter-option">horror</span>
+     				<span class="filter-option">drama</span>
+     				<span class="filter-option">science</span>
+     				<span class="filter-option">comedy</span>
+     				<span class="filter-option">action</span>
+     				<span class="filter-option">documentary</span>
+     				<span class="filter-option">fantasy</span>
+     				<span class="filter-option">musical</span>
+     				<span class="filter-option">sports</span>
+     				<span class="filter-option">romance</span>
+     				<span class="filter-option">thriller</span>
+     				<span class="filter-option">spy</span>
+     				<span class="filter-option">crime</span>
+     			</div>
      		</div>
-     	<?php }?>
-     </div>
- 	
- 	<div class="pagination">
- 		<?php
-    	for ($i = 1; $i <= $totalPages; $i++){
-    	   echo "<a href='?page=home&number=$i'>$i</a> ";
-    	}
-    	?>
+     		
+     		<div class="filter-section">
+     			<button class="filter-toggle">Status</button>
+     			<div class="filter-options">
+     				<span class="filter-option">movie</span>
+     				<span class="filter-option">serial</span>
+     				<span class="filter-option">cartoon movie</span>
+     				<span class="filter-option">cartoon series</span>
+     				<span class="filter-option">anime</span>
+     			</div>
+     		</div>
+     		
+     		<div class="filter-section">
+     			<button class="filter-toggle">Year</button>
+     			<div class="year-slider">
+     				<input type="number" id="year-min" value="1900" readonly>
+     				<div id="slider"></div>
+     				<input type="number" id="year-max" value="2024" readonly>
+     			</div>
+     		</div>
+     	</div>
+     	
+     	<div class="pagination">
+     		<?php
+        	for ($i = 1; $i <= $totalPages; $i++){
+        	   echo "<a href='?page=home&number=$i'>$i</a> ";
+        	}
+        	?>
+        </div>
  	</div>
 </div>
+<script src="scripts/JavaScript/filterOptions.js"></script>
---
+++ scripts/JavaScript/filterOptions.js
@@ -0,0 +1,14 @@
+function handleFilterClick(sectionClass) {
+	const section = document.querySelector(sectionClass);
+	const filterOptions = section.querySelectorAll('.filter-option');
+	
+	filterOptions.forEach(option => {
+		option.addEventListener('click', function() {
+			filterOptions.forEach(opt => opt.classList.remove('active'));
+			this.classList.add('active');
+		});
+	});
+}
+
+handleFilterClick('.filter-section:nth-child(1)');
+handleFilterClick('.filter-section:nth-child(2)');
