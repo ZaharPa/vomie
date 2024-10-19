31871f29ca197c9a116a97f425d3d474f1d28fcc
Zakhar Pashkevych <pashkevichzahar24@gmail.com>
2024-10-19 18:23:08 +0100

    create start for rate (2 part)

--- scripts/JavaScript/rate.js
+++ scripts/JavaScript/rate.js
@@ -2,38 +2,35 @@ const stars = document.querySelectorAll('.star');
 let selectedValue = 0;
 
 stars.forEach((star, index) => {
-	star.addEventListener('click', () => {
-		const value = index + 1;
-		selectedValue = value;
-		console.log(value);
+	star.addEventListener('click', (e) => {
+		const rect = e.currentTarget.getBoundingClientRect();
+		const mouseX = e.clientX - rect.left;
+		const halfValue = (mouseX < rect.width / 2) ? 0.5 : 1;
+		selectedValue = index + halfValue;
+		console.log(selectedValue);
+
+		updateStars(selectedValue);
+	});
+	
+	star.addEventListener('mouseover', (e) => {
+		const rect = e.currentTarget.getBoundingClientRect();
+ 		const mouseX = e.clientX - rect.left;
+		const halfValue = (mouseX < rect.width / 2) ? 0.5 : 1;
 		
-		updateStars();
+		updateStars(index + halfValue);
 	});
 	
-	star.addEventListener('mouseover', () => {
-		updateStars(index + 0.5);
-	});
-	
-	star.addEventListener('mouseout', () => {
-		updateStars();
-	});
 });
 
 function updateStars(hoverValue) {
 	stars.forEach((s, i) => {
-		s.classList.remove('full', 'active');
+		s.classList.remove('full', 'half');
 		if (hoverValue !== undefined) {
 			if (i < Math.floor(hoverValue)) {
 				s.classList.add('full');
+			} else if (i === Math.floor(hoverValue) && hoverValue % 1 !== 0) {
+				s.classList.add('half');
 			}
-			
-			if (i === Math.floor(hoverValue) && hoverValue % 1 !== 0) {
-				s.classList.add('active');
-			}
-		} else {
-			if (i < selectedValue) {
-				s.classList.add('full')
-			}
-		}
+		} 
 	});
 }
